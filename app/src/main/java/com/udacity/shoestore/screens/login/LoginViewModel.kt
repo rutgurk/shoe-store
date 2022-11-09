5ddbf8e9ba99f720a9979c28f2e9cfe0f9bb28f5
Rutger Korstjens <rutger.korstjens@gmail.com>
2022-11-09 20:17:01 +0100

    make sure isValidUsername rechecks when called

--- app/src/main/java/com/udacity/shoestore/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/screens/login/LoginViewModel.kt
@@ -13,15 +13,13 @@ class LoginViewModel : ViewModel() {
     val showError: LiveData<Boolean>
         get() = _showError
 
-    private val isValidUsername: Boolean = Utils.isValidEmail(username.value ?: "")
+    private fun isValidUsername(): Boolean = Utils.isValidEmail(username.value ?: "")
 
     fun clearUsernameError() {
         if (_showError.value == true) _showError.value = false
     }
 
     fun validateUsername() {
-        if (!isValidUsername) {
-            _showError.value = true
-        }
+        _showError.value = !isValidUsername()
     }
 }
